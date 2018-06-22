e836babca395972b662493333e87451b054cc8ac
EC2 Default User <ec2-user@ip-172-31-34-244.us-east-2.compute.internal>
2018-06-22 16:42:34 +0000

    add hangman identifing words and errors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,53 @@
 (function() {
-    let words, categoryChoice, wordChoice, el, memory, charValue, erro = 0;
+    let words, categoryChoice, wordChoice, el, memory, charValue, error = 0;
 
     words = {
         "ANIMAL": ["ELEFANTE", "CACHORRO", "GUAXINIM", "VACA"],
         "PROFISSÃO": ["PROFESSOR", "CARTEIRO", "DOUTOR", "ENGENHEIRO"]
     }
-/*
+
     categoryChoice = Math.floor((Math.random()*Object.keys(words).length));
     wordChoice = Math.floor((Math.random()*4));
 
     document.querySelector('#category').innerHTML = Object.keys(words)[categoryChoice];
 
     Object.keys(words)[categoryChoice] === "ANIMAL" ? memory = words["ANIMAL"][wordChoice] : memory = words["PROFISSÃO"][wordChoice];
-*/
 
-    memory = "VACA";
-    var teste = memory.replace(/./g,"_");
-    document.querySelector('#word').innerHTML = teste;
+    let preg = memory.replace(/./g,"_");
+    document.querySelector('#word').innerHTML = preg;
 
     el = document.getElementsByClassName('btn');
-    for(var x = 0; x <= el.length; x++) {
+    for(let x = 0; x <= el.length; x++) {
         el[x].addEventListener('click', function () {
             charValue = this.getAttribute('data-guide');
 
-                for(var i = 0; i < memory.length; i++) {
+            let position = [];
+            for(let w = 0; w < memory.length; w++) {
+              if(memory[w] === charValue) position.push(w);
+            }
 
-                    if(memory.indexOf(charValue) >= 0) {
-                        var pos = memory.indexOf(charValue);
-                        for(var x = 0; x <= document.querySelector('#word').childNodes.length; x++) {
-                            console.log(document.querySelector('#word').childNodes.item(x));
-                        }
-                    } else {
-                        alert('não achou nenhuma letra');
-                    }
+            if(position.length > 0) {
+                let letters = document.querySelector('#word').innerHTML;
+                for(let j = 0; j < position.length; j++) {
+                    letters = letters.substr(0, position[j]) + charValue + letters.substr(position[j] + 1);
+                    document.querySelector('#word').innerHTML = letters;
                 }
 
-            // if(charValue === memory.charAt()) {
-            //     document.querySelector('#word').innerHTML = memory.charAt(charValue);
-            // } else {
-            //     document.querySelector('#stickman').innerHTML = '<div class="error-'+ erro +'"></div>';
-            // }
-            // erro++
+            } else {
+                erro();
+            }
+
         });
+    }
+
+    function erro() {
+        let stick = document.querySelector('#stickman');
+
+        let div = document.createElement('div');
+        div.classList.add('error-' + error);
+        stick.appendChild(div);
+
+        error++;
     }
 
 })();
