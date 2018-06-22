4256bbd6c7e5b4d1aaea626de5079f5cdfbe3dd7
EC2 Default User <ec2-user@ip-172-31-34-244.us-east-2.compute.internal>
2018-06-22 14:03:03 +0000

    testing change _ to char

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,39 @@
 (function() {
     let words, categoryChoice, wordChoice, el, memory, charValue, erro = 0;
-    
-    words = {
-        "Animal": ["elefante", "cachorro", "guaxinim", "vaca"],
-        "Profissão": ["professor", "carteiro", "doutor", "engenheiro"]
-    }
 
+    words = {
+        "ANIMAL": ["ELEFANTE", "CACHORRO", "GUAXINIM", "VACA"],
+        "PROFISSÃO": ["PROFESSOR", "CARTEIRO", "DOUTOR", "ENGENHEIRO"]
+    }
+/*
     categoryChoice = Math.floor((Math.random()*Object.keys(words).length));
     wordChoice = Math.floor((Math.random()*4));
-    
+
     document.querySelector('#category').innerHTML = Object.keys(words)[categoryChoice];
 
-    Object.keys(words)[categoryChoice] === "Animal" ? memory = words["Animal"][wordChoice] : memory = words["Profissão"][wordChoice];
+    Object.keys(words)[categoryChoice] === "ANIMAL" ? memory = words["ANIMAL"][wordChoice] : memory = words["PROFISSÃO"][wordChoice];
+*/
 
-    document.querySelector('#word').innerHTML = memory.replace(/./g,"_");
-    
+    memory = "VACA";
+    var teste = memory.replace(/./g,"_");
+    document.querySelector('#word').innerHTML = teste;
+
     el = document.getElementsByClassName('btn');
     for(var x = 0; x <= el.length; x++) {
         el[x].addEventListener('click', function () {
             charValue = this.getAttribute('data-guide');
 
-            console.log(memory.charAt(2));
+                for(var i = 0; i < memory.length; i++) {
+
+                    if(memory.indexOf(charValue) >= 0) {
+                        var pos = memory.indexOf(charValue);
+                        for(var x = 0; x <= document.querySelector('#word').childNodes.length; x++) {
+                            console.log(document.querySelector('#word').childNodes.item(x));
+                        }
+                    } else {
+                        alert('não achou nenhuma letra');
+                    }
+                }
 
             // if(charValue === memory.charAt()) {
             //     document.querySelector('#word').innerHTML = memory.charAt(charValue);
