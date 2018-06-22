d55130cb6db257a8a5fad76d58648e03a473a5d5
Lucas <lucas@MacBook-Pro-de-Lucas.local>
2018-06-21 23:54:13 -0300

    add click function and replace words encode

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,34 @@
 (function() {
-    let words, categoryChoice, wordChoice;
-
+    let words, categoryChoice, wordChoice, el, memory, charValue, erro = 0;
+    
     words = {
-        "Animal": ["elefante", "águia", "guaxinim", "leão"],
-        "Profissão": ["professor", "secretária", "doutor", "engenheiro"]
+        "Animal": ["elefante", "cachorro", "guaxinim", "vaca"],
+        "Profissão": ["professor", "carteiro", "doutor", "engenheiro"]
     }
 
-    categoryChoice = Math.floor((Math.random()*2));
-
+    categoryChoice = Math.floor((Math.random()*Object.keys(words).length));
     wordChoice = Math.floor((Math.random()*4));
-
+    
     document.querySelector('#category').innerHTML = Object.keys(words)[categoryChoice];
 
-    console.log(Object.keys(words)[categoryChoice] === "Animal" ? words["Animal"][wordChoice] : words["Profissão"][wordChoice]);
+    Object.keys(words)[categoryChoice] === "Animal" ? memory = words["Animal"][wordChoice] : memory = words["Profissão"][wordChoice];
+
+    document.querySelector('#word').innerHTML = memory.replace(/./g,"_");
+    
+    el = document.getElementsByClassName('btn');
+    for(var x = 0; x <= el.length; x++) {
+        el[x].addEventListener('click', function () {
+            charValue = this.getAttribute('data-guide');
+
+            console.log(memory.charAt(2));
+
+            // if(charValue === memory.charAt()) {
+            //     document.querySelector('#word').innerHTML = memory.charAt(charValue);
+            // } else {
+            //     document.querySelector('#stickman').innerHTML = '<div class="error-'+ erro +'"></div>';
+            // }
+            // erro++
+        });
+    }
 
 })();
